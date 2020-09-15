52edeafec23bfa831e5bb6739c5433969770d3b9
Manish Kundu <35730135+iammanish17@users.noreply.github.com>
2020-09-16 01:21:53 +0530

    Create 1A.py

---
+++ binary search/1A.py
@@ -0,0 +1,71 @@
+# By manish.17, contest: ITMO Academy. Двоичный поиск - 1, problem: (A) Binary Search
+# https://codeforces.com/profile/manish.17
+
+from bisect import bisect_left, bisect_right
+
+class Result:
+    def __init__(self, index, value):
+        self.index = index
+        self.value = value
+
+class BinarySearch:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def greater_than(num: int, func, size: int = 1):
+        """Searches for smallest element greater than num!"""
+        if isinstance(func, list):
+            index = bisect_right(func, num)
+            if index == len(func):
+                return Result(None, None)
+            else:
+                return Result(index, func[index])
+        else:
+            alpha, omega = 0, size - 1
+            if func(omega) <= num:
+                return Result(None, None)
+            while alpha < omega:
+                if func(alpha) > num:
+                    return Result(alpha, func(alpha))
+                if omega == alpha + 1:
+                    return Result(omega, func(omega))
+                mid = (alpha + omega) // 2
+                if func(mid) > num:
+                    omega = mid
+                else:
+                    alpha = mid
+
+    @staticmethod
+    def less_than(num: int, func, size: int = 1):
+        """Searches for largest element less than num!"""
+        if isinstance(func, list):
+            index = bisect_left(func, num) - 1
+            if index == -1:
+                return Result(None, None)
+            else:
+                return Result(index, func[index])
+        else:
+            alpha, omega = 0, size - 1
+            if func(alpha) >= num:
+                return Result(None, None)
+            while alpha < omega:
+                if func(omega) < num:
+                    return Result(omega, func(omega))
+                if omega == alpha + 1:
+                    return Result(alpha, func(alpha))
+                mid = (alpha + omega) // 2
+                if func(mid) < num:
+                    alpha = mid
+                else:
+                    omega = mid
+
+import sys
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+bs = BinarySearch()
+print("\n".join("YES" if bs.greater_than(k-1, a).value == k else "NO" for k in b))
